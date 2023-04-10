e142c06fa315b72546ae92c0ffe4fa847ffc6f94
divyaporwal-hub <divyaporwal100@gamil.com>
2023-04-10 20:29:07 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import temp from "./components/weather/index";
-// import {UseState} from "./component/Hooks/useState";
-// import {UseEffect} from "./component/Hooks/useEffect";
-// import UseReducer from "./components/Hooks/useReducer";
 
 import Temp from "./Weather/temp";
 
